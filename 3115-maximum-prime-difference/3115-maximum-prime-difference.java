a953e44a2b58bebcfa312c5b4841328091a3d6a1
sreekanth402 <ksreekanth984@gmail.com>
2026-09-23 14:41:03 +0530

    Time: 11 ms (52.21%), Space: 115.8 MB (89.47%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.java
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.java
@@ -26,7 +26,7 @@ class Solution {
             return false;
         }
 
-        for(int i = 2; i <= Math.sqrt(n); i++) {
+        for(int i = 2; i*i <= n; i++) {
 
             if(n % i == 0) {
                 return false;
